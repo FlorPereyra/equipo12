de34f1bb1e529badb6cc0437c00a71baca87fd76
FlorenciaPereyra <pereyraramosflorenciacamila@gmail.com>
2023-05-28 00:05:46 -0300

    agregado de archivos

---
+++ addTareasEquipo12.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+read -p "Ingrese su la tarea a agregar: " tarea
+
+echo $tarea >> tareasDiarias.txt
---
+++ delTareasEquipo12.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+read -p "Ingrese el numero de tareas a eliminar: " numero
+
+lineasDelArchivo=$(wc -l < tareasDiarias.txt)
+eliminarDesde=$((lineasDelArchivo-numero+1))
+
+sed -i ''$eliminarDesde', $d' tareasDiarias.txt
